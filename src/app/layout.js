de5c82965138d771b7c8dc517bc2d594a3861846
amiamitswe <amiamitswe@gmail.com>
2025-07-29 23:34:14 +0600

    feat: configure theme

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,17 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Figtree,Urbanist } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+
+const figtree = Figtree({
+  variable: "--font-figtree",
   subsets: ["latin"],
+  weight: ["400", "500", "600", "700","800"], 
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const urbanist = Urbanist({
+  variable: "--font-urbanist",
   subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800"],
 });
 
 export const metadata = {
@@ -20,7 +23,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${figtree.variable} ${urbanist.variable} antialiased`}
       >
         {children}
       </body>
